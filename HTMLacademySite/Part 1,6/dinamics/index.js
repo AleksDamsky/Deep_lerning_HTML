db9b6acae3cd2511b43a89b133d459e22a19b260
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-14 22:31:40 +0300

    to paste new gifka

---
+++ HTMLacademySite/Part 1,6/dinamics/index.js
@@ -0,0 +1,9 @@
+let i = {};
+
+i = aleks;
+
+let aleks = [nikita, anton, ivan];
+let nikita = ["denis", "dzadza", "bulat"];
+let anton = ["semen", "boris", "sergey"];
+let ivan = ["danil", "salim", "igor"];
+console.log(i);
