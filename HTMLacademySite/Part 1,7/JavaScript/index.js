3f0280ff453608588c7b97513225a82257c62f99
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-15 20:58:49 +0300

    1,7 mainCSS

--- HTMLacademySite/Part 1,7/JavaScript/index.js
+++ HTMLacademySite/Part 1,7/JavaScript/index.js
@@ -1,1 +1,10 @@
 // Тут постараюсь решить задачу FizzBuzz
+// let someFunc = function (num) {
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 3 === 0 && i % 5 === 5) {
+//       console.log("fizzbuzz");
+//     } else if(i % 3 === 0) {
+
+//     }
+//   }
+// };
