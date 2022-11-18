e1921d74d50901b2521558fcfd2dec05e22e9bdd
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-18 04:47:23 +0300

    fizzBuzz

--- HTMLacademySite/Part 1,7/JavaScript/index.js
+++ HTMLacademySite/Part 1,7/JavaScript/index.js
@@ -1,10 +1,15 @@
-// Тут постараюсь решить задачу FizzBuzz
-// let someFunc = function (num) {
-//   for (let i = 1; i <= num; i++) {
-//     if (i % 3 === 0 && i % 5 === 5) {
-//       console.log("fizzbuzz");
-//     } else if(i % 3 === 0) {
+let fizzBuzz = (num) => {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
 
-//     }
-//   }
-// };
+fizzBuzz(22);
