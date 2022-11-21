227773142474b10a8bc045c6101bd67555da407d
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-21 21:18:37 +0300

    js

--- HTMLacademySite/Part 1,9/javaScript/index.js
+++ HTMLacademySite/Part 1,9/javaScript/index.js
@@ -2,8 +2,44 @@
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
-function getElement(massiv) {
-  let a = [];
-  for (let i = 0; i <= massiv.length; i++) {}
-}
-getElement([1, 2, "a", "b"]);
+// function getElement(massiv) {
+//   let a = [];
+//   for (let i = 0; i <= massiv.length; i++) {}
+// }
+// getElement([1, 2, "a", "b"]);
+
+let massiv = ["lol", "kek", "bob", "roy", "fri"];
+
+let massiv2 = ["2", "3", "5"];
+
+console.log(massiv);
+
+massiv.pop();
+
+console.log(massiv);
+
+massiv.push("dick");
+
+console.log(massiv);
+
+massiv.shift();
+
+console.log(massiv);
+
+massiv.unshift("gerda");
+
+console.log(massiv);
+
+console.log(massiv.length);
+
+let obj = massiv.concat(massiv2);
+
+console.log(obj);
+
+massiv.splice(4, 0, "chum");
+
+console.log(massiv);
+
+massiv.slice(1, 2);
+
+console.log(massiv);
