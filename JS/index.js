b24d4033f99f36ab67593e33803efe7c60d08957
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-12-01 04:21:10 +0300

    learn Js

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,20 @@
-function multiply(a, b) {
-  let c = a * b;
-  console.log(c);
-}
+// function multiply(a, b) {
+//   let c = a * b;
+//   console.log(c);
+// }
 
-multiply(2, 3);
+// multiply(2, 3);
+
+// Получилась функция, которая принимает массив и сортирует его от самого меньшего до самого большего.
+function parseArray(array) {
+  let newArray = array.sort((a, b) => a - b);
+  //   console.log(newArray);
+
+  console.log(newArray[newArray - 1]);
+
+  //   let strArray = newArray.join(" ");
+  //   console.log(strArray);
+
+  //   console.log(strArray[strArray.lenght - 1]);
+}
+parseArray([2, 5, 6, 12, -23]);
