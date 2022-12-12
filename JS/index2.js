fe3b1a1781533fcde61f6555b1afae26c16782b2
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-12-12 14:43:19 +0300

    make tetris

---
+++ JS/index2.js
@@ -0,0 +1,4 @@
+//создаем переменную, для пользования хослтом
+const holst = document.getElementById("game");
+//создаем переменную для того, чтобы показать, какой вид игры у нас будет
+const render = canvas.getContext("2d");
