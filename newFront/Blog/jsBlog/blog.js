60627ac71a57d1d533f3514999473556fc12be3a
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2023-01-03 17:07:50 +0300

    check

--- newFront/Blog/jsBlog/blog.js
+++ newFront/Blog/jsBlog/blog.js
@@ -1,29 +1,54 @@
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11];
-console.log(a);
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11];
+// console.log(a);
 
-let b = a.reduce((a, b) => b - a);
-console.log(b);
+// let b = a.reduce((a, b) => b - a);
+// console.log(b);
 
-let c = a.sort((a, b) => b - a);
-console.log(c);
+// let c = a.sort((a, b) => b - a);
+// console.log(c);
 
-let ar = ["rules", "for", "arr"];
-console.log(ar);
+// let ar = ["rules", "for", "arr"];
+// console.log(ar);
 
-function getElem(elem, index, array) {
-  let z = "lol";
-  return elem === z;
+// function getElem(elem, index, array) {
+//   let z = "lol";
+//   return elem === z;
+// }
+
+// console.log(ar.find(getElem));
+
+// let t = 9;
+// let num = a.sort((a, b) => a - b).indexOf(t); //два метода потому что мы сначала массив развернули. А ща перевернули обратно и нашли нужный элемент
+// console.log(num);
+
+// let j = ar.join("-");
+// console.log(j);
+
+//заменить на стрелочную функцию
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
 }
 
-console.log(ar.find(getElem));
+ask(
+  "Вы согласны?",
+  function () {
+    alert("Вы согласились.");
+  },
+  function () {
+    alert("Вы отменили выполнение.");
+  }
+);
 
-let t = 9;
-let num = a.sort((a, b) => a - b).indexOf(t); //два метода потому что мы сначала массив развернули. А ща перевернули обратно и нашли нужный элемент
-console.log(num);
+let ask = (question, yes, no) => {
+  switch (true) {
+    case confirm(question):
+      yes();
+      break;
+    default:
+      no();
+  }
+};
 
-let j = ar.join("-");
-console.log(j);
-
-let r = ar[0].replace("lol");
-console.log(r);
-console.log(ar);
+// example
+//chck
